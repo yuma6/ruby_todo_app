6bc81569d86dd4d2574e3b2180ff31d3e98e3d66
yumayumayuma <yumapuripuri@gmail.com>
2021-09-14 14:21:07 +0900

    投稿と編集まで

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,9 +1,15 @@
-class Task < ActionController::Base
+class TaskController < ActionController::Base
 
     def index
+        @task = Task.all.order(created_at: :desc)
     end
 
     def show
+        @task = Task.find_by(id: params[:id])
+    end
+
+    def edit
+        @task = Task.find_by(id: params[:id])
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root "task#index"
+
   get "task/index" => "task#index"
   get "task/:id/show" => "task#show"
+  get "task/:id/edit" => "task#edit"
+
+  post "post/new" => "post#new"
+  post "post/:id/edit" => "post#edit"
+  post "post/:id/destroy" => "post#destroy"
 end
