6736895862f8179123cee3d13aebe50ecffda0f4
yumayumayuma <yumapuripuri@gmail.com>
2021-10-01 13:27:10 +0900

    beginning_of_week

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+    before_action :beginning_of_week
+
+    private
+  
+      def beginning_of_week
+        Date.beginning_of_week = :sunday
+      end
+  
 end
