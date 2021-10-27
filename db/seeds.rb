d73de0127932dd9da098dbbdef230c73fb9d0cf6
yumayumayuma <yumapuripuri@gmail.com>
2021-10-27 11:22:04 +0900

    user_id_wrong&add_seeds_task

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,8 +12,50 @@
         password: "example#{n+1}"
     )
 end
+
 5.times do |n|
     Team.create!(
-        team_name: "test-team#{n+1}",
+        team_name: "test-team#{n+1}"
+    )
+end
+
+5.times do |n|
+    Task.create!(
+        content: "user1-team#{n+1}",
+        user_id: "1",
+        team_id: "#{n+1}",
+        start_time: "2021-10-27"
+    )
+end
+5.times do |n|
+    Task.create!(
+        content: "user2-team#{n+1}",
+        user_id: "2",
+        team_id: "#{n+1}",
+        start_time: "2021-10-27"
+    )
+end
+5.times do |n|
+    Task.create!(
+        content: "user3-team#{n+1}",
+        user_id: "3",
+        team_id: "#{n+1}",
+        start_time: "2021-10-27"
+    )
+end
+5.times do |n|
+    Task.create!(
+        content: "user4-team#{n+1}",
+        user_id: "4",
+        team_id: "#{n+1}",
+        start_time: "2021-10-27"
+    )
+end
+5.times do |n|
+    Task.create!(
+        content: "user5-team#{n+1}",
+        user_id: "5",
+        team_id: "#{n+1}",
+        start_time: "2021-10-27"
     )
 end
