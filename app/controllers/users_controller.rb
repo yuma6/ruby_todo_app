df6f37e35328450ef6d41134f54a9745d1463c99
yumayumayuma <yumapuripuri@gmail.com>
2021-11-10 14:44:44 +0900

    user_name

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+    before_action :set_current_user,only:[:name]
+
+    def name
+        @user.name = params[:name]
+        if @user&.name.present?
+          @user.save
+          flash[:notice]="ユーザー名を設定しました"
+        else
+          flash[:alert]="ユーザー名が入力されていません"
+        end
+        redirect_back(fallback_location: root_path)
+    end
+
+end
