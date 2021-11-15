efef63d6c2981e95a02123eb8de529de7aac7be6
yumayumayuma <yumapuripuri@gmail.com>
2021-11-15 14:50:51 +0900

    heroku_push_schema_date

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_12_061901) do
+ActiveRecord::Schema.define(version: 2021_11_11_025628) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "content"
@@ -19,6 +19,21 @@ ActiveRecord::Schema.define(version: 2021_10_12_061901) do
     t.boolean "finish"
     t.date "start_time"
     t.integer "user_id"
+    t.integer "team_id"
+  end
+
+  create_table "team_users", force: :cascade do |t|
+    t.integer "team_id"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string "team_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "manager_id", default: 1
   end
 
   create_table "users", force: :cascade do |t|
@@ -29,6 +44,8 @@ ActiveRecord::Schema.define(version: 2021_10_12_061901) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "deve_user", default: false, null: false
+    t.string "name", default: ""
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
